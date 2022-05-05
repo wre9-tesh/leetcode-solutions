0f1310f61e421c003f30e526b264e2db4121b2a1
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-05-05 23:37:07 +0530

    Time: 1 ms (64.30%), Space: 42 MB (56.58%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,10 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+         char[] a = s.toCharArray();
+        for(int i=0; i < a.length;i++){
+            if('A' <= a[i] && a[i] <= 'Z'){
+            a[i]=(char)(a[i] + 32);}
+        }
+       return new String(a); 
     }
 }

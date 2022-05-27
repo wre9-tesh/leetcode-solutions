f1378a289bba02fa03b45d142c68b18335806998
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-05-28 00:43:28 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (11.08%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,9 +1,33 @@
 class Solution {
     public int strStr(String haystack, String needle) {
- 
-        
-    int output = haystack.indexOf(needle);
-        
-        return output;
+        if(haystack.contains(needle)){
+            
+            for(int i=0; i<haystack.length();i++){
+                
+                if(needle.charAt(0) == haystack.charAt(i) ){
+                    if(isSame(haystack,needle,i)) {
+                        return i;
+                    }
+                    else{
+                        continue;
+                    }
+                       
+                }      
+            
+            }         
+        }
+           
+   
+        return -1;
     }
+    boolean isSame(String hay,String needle,int j){
+		    for(int i=0;i<needle.length();i++){
+                   if( j < hay.length() && hay.charAt(j) != needle.charAt(i)){
+                       return false;
+                   }
+                j++;
+            }
+        return true;
+	}
+
 }

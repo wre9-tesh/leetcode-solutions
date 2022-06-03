755a70016803848c3138d7442bc23f03a7c874eb
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-06-04 00:17:56 +0530

    Time: 11 ms (40.68%), Space: 54.2 MB (57.41%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,38 +1,33 @@
 class Solution {
     public boolean validPalindrome(String s) {
-     int i=0,j=s.length()-1;
-        
+            
+        int i=0;
+        int j=s.length()-1;
         
         while(i<j){
-            
-            if(s.charAt(i) != s.charAt(j)){
-                return (isPalindrome(s,i,j-1) || isPalindrome(s,i+1,j));
-            }
-            
-            
+  
+        if(s.charAt(i)!=s.charAt(j)){
+            return isPaliornot(s,i,j-1) || isPaliornot(s,i+1,j);
+        }    
             i++;
             j--;
         }
         
-     
-        
-    return true;
+       return true; 
     }
-     public boolean isPalindrome(String s,int i,int j) {
-     
+    
+    boolean isPaliornot(String s,int i,int j){
+            
         
         while(i<j){
-            char start=s.charAt(i);
-            char end=s.charAt(j);
         
-            if(start!=end){
+        if(s.charAt(i)!=s.charAt(j)){
             return false;
-            }
-                i++;
-                j--;
-            
+        }    
+            i++;
+            j--;
         }
         return true;
-        
     }
+    
 }

1fa23704f641f4c34d6b11ff030ce496adac6097
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-06-06 17:51:42 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (31.60%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -3,15 +3,18 @@ class Solution {
     if (strs.length == 0) return "";
      
      String prefix = strs[0];
+        
+        for(int i=1 ; i<strs.length;++i){
+            while(strs[i].indexOf(prefix)!= 0){
+                prefix = prefix.substring(0,prefix.length()-1);
+                    if(prefix.isEmpty()){
+                        return "";
+                    }
+                
+            }
+        }
      
-     for(int i= 1;i<strs.length;i++){
-         while(strs[i].indexOf(prefix)!=0){
-             prefix = prefix.substring(0,prefix.length()-1);
-             if(prefix.isEmpty()){
-                 return "";
-             }
-         }
-     }
+     
      
      
 return prefix;

a61d649f9202858459c609b74c1fa2ebea251bb8
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-06-07 23:21:11 +0530

    Time: 1 ms (54.20%), Space: 43.3 MB (21.61%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.java
@@ -0,0 +1,18 @@
+class Solution {
+   public int wateringPlants(int[] p, int capacity) {
+        int temp =capacity;
+        int steps =0;
+       for(int i=0 ; i< p.length ;i++){
+            
+           if(p[i] > temp ){
+               steps += i*2;
+               temp =capacity;
+           }
+           temp -= p[i];
+           steps++;
+       }
+       
+       
+    return steps;   
+}
+}

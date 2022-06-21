c372eb1ca8196b4d3a77b4e619f1b02df2b1cad1
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-06-21 17:46:01 +0530

    Time: 10 ms (98.14%), Space: 58.4 MB (80.22%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,19 +1,29 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] P, String S) {
-        
-  Arrays.sort(P);
+        Arrays.sort(P);
         List<List<String>> ans = new ArrayList<>();
-        int left = 0, right = P.length - 1;
-        for (int i = 0; i < S.length(); i++) {
+    
+        int left = 0;
+        int right = P.length - 1;
+        for(int i=0 ; i<S.length() ;i++ ){
             List<String> res = new ArrayList<>();
             char c = S.charAt(i);
-            while (left <= right && (P[left].length() == i || P[left].charAt(i) < c)) left++;
-            while (left <= right && (P[right].length() == i || P[right].charAt(i) > c)) right--;
-            for (int j = 0; j < 3 && left + j <= right; j++)
-                res.add(P[left+j]);
-            ans.add(res);
+         
+         while(left <=right && (P[left].length()== i ||P[left].charAt(i) < c)){
+                left++;
+         }   
+          while(left <=right && (P[right].length()== i ||P[right].charAt(i) > c)){
+                right--;
+         }
+            
+         for(int j=0;j<3 && left+j<=right;j++){
+            res.add(P[j+left]);    
+    
+         }   
+            
+           ans.add(res); 
         }
-        return ans;
         
+return ans;
     }
 }

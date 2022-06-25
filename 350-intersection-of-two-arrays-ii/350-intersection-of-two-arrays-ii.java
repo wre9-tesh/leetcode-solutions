9198914ad95926830d6780536aa3451a99d07884
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-06-25 14:19:52 +0530

    Time: 4 ms (66.42%), Space: 43.1 MB (78.90%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,35 +1,34 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
+    
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        for(int i:nums2){
-            int x=binarySearch(nums1,i,ans);
-        }
-        int[] arr = ans.stream().mapToInt(i -> i).toArray();
-        return arr;
 
-            
-    }
-    public int binarySearch(int[] arr, int target,ArrayList<Integer> ans) {
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                ans.add(arr[mid]);
-                arr[mid]=-1;
-                Arrays.sort(arr);
-                break;  
-
+        int i = 0,j =0;
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        while(i < nums1.length && j<nums2.length ){
+           
+            if(nums1[i] < nums2[j]){
+                i++;
             }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                 ans.add(nums1[i]);
+                i++;
+                j++;
+               
+            }
+            
         }
-        return -1;
+     
+	int[] g = new int[ans.size()];
+	for (int k = 0; k < ans.size(); k++) {
+		g[k] = ans.get(k);
+	}
+	return g;
     }
-
 }

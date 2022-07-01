701cdf6e6ec2c05c167160f1c45e17609f655f54
amitesh kumar <96977189+wre9-tesh@users.noreply.github.com>
2022-07-01 14:49:32 +0530

    Time: 79 ms (12.92%), Space: 81.4 MB (21.63%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -2,32 +2,41 @@ class Solution {
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
         
-        
-        int start=0;
+        int start =0;
         int end=position[position.length-1];
-        int ans=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
+        int ans =0;
+        
+        while(start <= end){
+            int mid = start+(end-start)/2;
             
             if(canplace(position,m,mid)){
-                ans=mid;
-                start=mid+1;
+                ans =mid;
+                start = mid+ 1;
+                
             }
+            
             else{
-            end=mid-1;}
+                end = mid-1;
+            }
+            
+            
         }
- return ans;
+        
+    return ans;
     }
     public boolean canplace(int[] nums,int m ,int mid){
-        int curr=nums[0];
-            int count=1;
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]-curr >= mid){
-                curr=nums[i];
-                count++;
+       
+        int place =1;
+        int prev =nums[0];
+        for(int i=0;i<nums.length;i++){
+            
+            if(nums[i] - prev >= mid){
+                prev= nums[i];
+                place ++;
+            
             }
         }
         
-        return  count >= m;
+     return place >= m;
     }
 }

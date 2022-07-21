a81dd258faff2f9c9fd1b076a6832b23570432f8
amitesh kumar <amiteshptn@gmail.com>
2022-07-21 19:41:03 +0530

    Time: 1 ms (60.67%), Space: 41 MB (40.00%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
         
-        
-        int y = n >> 1;
-        
-        int a = (n ^ y) + 1 ;
 
-       return (a & (a-1)) == 0; 
+       return (((n ^ (n >> 1)) + 1) & ((n ^ (n >> 1)))) == 0; 
     }
 }

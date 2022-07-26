39935f46a240dfb75b845d1084b8aab0b4f5fa16
amitesh kumar <amiteshptn@gmail.com>
2022-07-26 21:39:58 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (29.70%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,28 +1,53 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int i = nums1.length - 1;
-        int j = 0;
-        
-        
-      
-        
-        while( i >= 0 && nums1[i] == 0 && j < nums2.length ){
-                 swap(nums1,nums2,i,j); 
-            i--;
-            j++;
+     
+        if(m == 0 || n == 0){
+            if(m==0){
+                nums1[0] = nums2[0];
+            }
+            else{
+               return;
+            }
+
         }
         
-     
-        Arrays.sort(nums1);
         
         
         
-    }
-    public void swap(int[] n1 ,int[] n2 ,int a, int b){
-            int temp=n1[a];
-                n1[a]=n2[b];
-                n2[b]=temp;
+        int[] mix = new int[nums1.length];
+        
+        int i=0,j=0,k=0;
+        
+        while(i<m && j<n){
+            
+            if(nums1[i] < nums2[j]){
+                mix[k] = nums1[i];
+                i++;
+            }
+            else{
+                mix[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(j<n){
+            mix[k] = nums2[j];
+            k++;
+            j++;
+        }
+        while(i<m){
+            mix[k] = nums1[i];
+            k++;
+            i++;
+        }
+        
+        for(int l=0;l<m+n;l++){
+            
+            nums1[l] = mix[l];
+            
+        }
+        
         
     }
 }

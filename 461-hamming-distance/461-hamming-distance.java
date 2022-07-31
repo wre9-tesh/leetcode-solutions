2bf8ebcb124d55191fa66191fbb6628b9d044d5c
amitesh kumar <amiteshptn@gmail.com>
2022-07-31 19:19:13 +0530

    Time: 1 ms (45.80%), Space: 41.1 MB (43.95%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,16 +1,5 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int count=0;
-        
-        for(int i =0 ;i<32;i++){
-            
-            if( ( (x >> i) & 1) != ((y >> i) & 1)){
-                count++;
-            }
-            
-            
-        }
-        
-        return count;
+      return Integer.bitCount(x ^ y);
     }
 }

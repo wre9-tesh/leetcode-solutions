5b9099b9a70cd7cea51b2fcdb9178b3c798786bd
amitesh kumar <amiteshptn@gmail.com>
2022-08-01 21:41:07 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (71.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,20 +1,22 @@
 class Solution {
     public int uniquePaths(int z, int y) {
         
+       int total_move = z + y - 2;
+       
+       int k = Math.min(z-1,y-1);
+       
+    long res = 1;
         
-        int n = y+z-2;
-        int k= Math.min(z-1,y-1);
+       for(int i =1 ; i<=k ;i++ ){
+           
+           res = res * (total_move -k+i)/i ;
+           
+       } 
         
         
-        long res = 1;
-        for(int i = 1 ; i<=k ;i++ ){
-
-            res = res * (n-k +i)/i;
-            
-        }
         
         
-        return (int)res;
+        return (int) res; 
     }
 
 }

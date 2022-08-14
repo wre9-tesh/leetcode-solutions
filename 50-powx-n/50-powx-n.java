dcb900269d73b7f75477a76738367061877e7fb0
amitesh kumar <amiteshptn@gmail.com>
2022-08-14 18:51:23 +0530

    Time: 1 ms (89.32%), Space: 44.6 MB (7.55%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,7 +1,28 @@
 class Solution {
     public double myPow(double x, int n) {
+     
+        if(x == 1){
+            return 1;
+        }
         
-       return Math.pow(x,n);
         
+        double ans = helper(x,n);
+        return  n > 0 ? ans: 1/ans;
     }
+    
+     public double helper(double x, int n){
+               
+        int exp = Math.abs(n);
+         
+             if(exp == 0 )
+             {    
+             return 1;
+             }
+           
+             if(exp % 2 == 0){
+             return helper(x*x,exp/2);
+             }
+        
+          return  x * (helper(x*x,(exp-1)/2));
+     }         
 }

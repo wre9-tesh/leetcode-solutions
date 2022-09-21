0be6055a39783456f802b9fa9e17e827cd33382d
amitesh kumar <amiteshptn@gmail.com>
2022-09-21 15:26:48 +0530

    Time: 103 ms (45.21%), Space: 44.2 MB (64.45%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -1,17 +1,2 @@
-/**
- * @param {string[]} sentences
- * @return {number}
- */
-var mostWordsFound = function(sentences) {
-    
-    let max =0;
-    
-    for(let i=0;i<sentences.length;i++){
-        
-        let ans = sentences[i].split(' ');
-        
-        max = Math.max(ans.length,max);
-    }
-    
-    return max;
-};
+
+const mostWordsFound = (sentences) => sentences.reduce((num, el) => Math.max(num, el.split(' ').length), 0);
